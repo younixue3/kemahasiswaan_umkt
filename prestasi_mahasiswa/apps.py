f3797f404312f9144f675e16497135ee6d33fa88
younixue3 <bussiness.needs.rctka@gmail.com>
2022-05-29 08:30:44 +0800

    Fixing

---
+++ prestasi_mahasiswa/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PrestasiMahasiswaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'prestasi_mahasiswa'
---
+++ templates/kemahasiswaan_umkt/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+      "./index.html",
+      "./src/**/*.{html,js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
