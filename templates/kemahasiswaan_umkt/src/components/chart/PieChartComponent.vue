ce3bda0f5afbf8d08d25a95eec8fa7693bfb6412
younixue3 <bussiness.needs.rctka@gmail.com>
2022-05-30 19:38:18 +0800

    Fixing

---
+++ templates/kemahasiswaan_umkt/src/components/chart/PieChartComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <Pie :chart-data="chartData"/>
+</template>
+
+<script>
+import {Pie} from 'vue-chartjs/legacy'
+import {Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, ArcElement} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, ArcElement)
+
+export default {
+  name: 'PieChartComponent',
+  components: {Pie},
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    },
+    chartOptions: {
+      type: Object,
+      required: true
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    }
+  }
+}
+</script>
--- templates/kemahasiswaan_umkt/src/views/dashboard/DashboardPage.vue
+++ templates/kemahasiswaan_umkt/src/views/dashboard/DashboardPage.vue
@@ -3,17 +3,17 @@
     <div class="grid grid-cols-12 gap-x-5 gap-y-5 my-5">
       <MacCardComponent cardsname="Staff | Status" class="row-span-2 col-span-6 overflow-hidden">
         <div class="">
-          <BarChartComponent :chart-data="{'labels': ['test', 'test2', 'test3', 'test4'], 'datasets': [{ 'data': [200, 20, 12, 20, 12] }]}" :chart-options="{responsive: true,maintainAspectRatio: false,'indexAxis': 'y'}"/>
+          <BarChartComponent :chart-data="{'labels': ['test', 'test2', 'test3', 'test4'], 'datasets': [{label: 'Nilai Prestasi 10 Teratas', 'data': [200, 20, 12, 20, 12] }]}" :chart-options="{responsive: true,maintainAspectRatio: false,'indexAxis': 'y'}"/>
         </div>
       </MacCardComponent>
       <MacCardComponent cardsname="Staff | Status" class="row-span-1 col-span-6 overflow-hidden">
         <div class="">
-          <BarChartComponent :chart-data="{'labels': ['test', 'test2', 'test3', 'test4'], 'datasets': [{ 'data': [200, 20, 12, 20, 12] }]}" :chart-options="{responsive: true,maintainAspectRatio: false,'indexAxis': 'y'}" :height="300"/>
+          <BarChartComponent :chart-data="{'labels': ['test', 'test2', 'test3', 'test4'], 'datasets': [{label:'Nilai Per Jenis Prestasi', 'data': [200, 20, 12, 20, 12] }]}" :chart-options="{responsive: true,maintainAspectRatio: false,'indexAxis': 'x'}" :height="300"/>
         </div>
       </MacCardComponent>
       <MacCardComponent cardsname="Staff | Status" class="row-span-1 col-span-6 overflow-hidden">
         <div class="">
-          <BarChartComponent :chart-data="{'labels': ['test', 'test2', 'test3', 'test4'], 'datasets': [{ 'data': [200, 20, 12, 20, 12] }]}" :chart-options="{responsive: true,maintainAspectRatio: false,'indexAxis': 'y'}" :height="300"/>
+          <PieChartComponent :chart-data="{'labels': ['test', 'test2', 'test3', 'test4'], 'datasets': [{label: 'Nilai Prestasi berdasarkan Prodi', 'data': [200, 20, 12, 20, 12] }]}" :chart-options="{responsive: true,maintainAspectRatio: false}" :height="300"/>
         </div>
       </MacCardComponent>
     </div>
@@ -23,9 +23,11 @@
 <script>
 import MacCardComponent from "@/components/widget/MacCardComponent";
 import BarChartComponent from "@/components/chart/BarChartComponent";
+import PieChartComponent from "@/components/chart/PieChartComponent";
 export default {
   name: "DashboardPage",
   components: {
+    PieChartComponent,
     BarChartComponent,
     MacCardComponent
   }
