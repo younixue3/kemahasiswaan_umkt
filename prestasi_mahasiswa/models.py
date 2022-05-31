9303d5ecb8118d989c76c6cdd2626c3a1c6bcaf6
younixue3 <bussiness.needs.rctka@gmail.com>
2022-06-01 06:19:52 +0800

    Fixing

--- prestasi_mahasiswa/models.py
+++ prestasi_mahasiswa/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class prestasi(models.Model):
+    TIM = 'tim'
+    INDIVIDU = 'individu'
+    TIM_INDIVIDU_CHOICES = [
+        (TIM, 'Tim'),
+        (INDIVIDU, 'Individu'),
+    ]
+    nama_event = models.CharField(max_length=80)
+    url_kegiatan = models.TextField()
+    penyelenggara = models.CharField(max_length=80)
+    lingkup_tingkat = models.CharField(max_length=50)
+    jumlah_negara = models.IntegerField()
+    kategori = models.CharField(max_length=50)
+    prestasi_diraih = models.CharField(max_length=80)
+    ekuivalensi = models.CharField(max_length=80)
+    tempat = models.TextField()
+    tanggal_mulai = models.DateField()
+    tanggal_selesai = models.DateField()
+    deskripsi = models.TextField()
+    bukti = models.TextField()
+    foto_kegiatan = models.TextField()
+    tim_individu = models.CharField(
+        max_length=10,
+        choices=TIM_INDIVIDU_CHOICES,
+        default=INDIVIDU
+    )
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.user.username
