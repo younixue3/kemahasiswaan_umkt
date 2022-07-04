a108011a1c79e57cccfc8a1625b4fcfe1256ccf9
Younixue3 <bussiness.needs.rctka@gmail.com>
2022-07-04 20:18:27 +0800

    Fixing Route Login Redirect

--- templates/kemahasiswaan_umkt/src/views/auth/LoginPage.vue
+++ templates/kemahasiswaan_umkt/src/views/auth/LoginPage.vue
@@ -1,29 +1,6 @@
 <template>
   <div>
-    <form @submit.prevent="login"
-          class="absolute bg-white shadow-lg border border-gray-300 rounded-xl w-72 md:w-96 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-      <div class="my-10">
-        <div class="text-4xl font-bold">Welcome</div>
-        <div class="text-lg font-light leading-3">Sign in to your account</div>
-      </div>
-      <div class="mx-5 my-10">
-        <input type="text"
-               class="flex-1 block w-full rounded-t-xl focus:outline-none px-3 py-1.5 border border-b-0 border-gray-300"
-               placeholder="NIM" name="nim" v-model="nim">
-        <input type="password"
-               class="flex-1 block w-full rounded-b-xl focus:outline-none px-3 py-1.5 border border-gray-300"
-               placeholder="Password" name="password" v-model="password">
-      </div>
-      <div class="mx-5 my-10">
-        <div class="m-auto flex space-x-5">
-          <button class="bg-indigo-500 text-xl text-white text-center rounded-md shadow-md w-full px-5 py-1"><span
-              class="align-text-top">Sign in</span></button>
-          <button
-              class="text-xl text-indigo-500 text-center rounded-md shadow-md border border-gray-100 w-full px-5 py-1">
-            <span class="align-text-top">Sign up</span></button>
-        </div>
-      </div>
-    </form>
+
   </div>
 </template>
 
